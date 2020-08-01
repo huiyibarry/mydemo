8f8c3f7fc33494929eb177e547ef52363a1b61b3
huiyibarry <1373949185@qq.com>
2020-08-01 21:54:25 +0800

    公共文件

--- application/common.php
+++ application/common.php
@@ -10,3 +10,106 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+
+//随机加密串
+function randstr($length=6,$special=true){
+    $hash='';
+    $chars= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz'; 
+    $special && $chars .= '@#!~?:-=';
+    $max=strlen($chars)-1;   
+    mt_srand((double)microtime()*1000000);   
+    for($i=0;$i<$length;$i++)   {   
+    $hash.=$chars[mt_rand(0,$max)];   
+    }
+    return $hash;
+}
+
+function GetIp() {
+    $realip = '';
+    $unknown = 'unknown';
+    if (isset($_SERVER)) {
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) && strcasecmp($_SERVER['HTTP_X_FORWARDED_FOR'], $unknown)) {
+            $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            foreach ($arr as $ip) {
+                $ip = trim($ip);
+                if ($ip != 'unknown') {
+                    $realip = $ip;
+                    break;
+                }
+            }
+        } else if (isset($_SERVER['HTTP_CLIENT_IP']) && !empty($_SERVER['HTTP_CLIENT_IP']) && strcasecmp($_SERVER['HTTP_CLIENT_IP'], $unknown)) {
+            $realip = $_SERVER['HTTP_CLIENT_IP'];
+        } else if (isset($_SERVER['REMOTE_ADDR']) && !empty($_SERVER['REMOTE_ADDR']) && strcasecmp($_SERVER['REMOTE_ADDR'], $unknown)) {
+            $realip = $_SERVER['REMOTE_ADDR'];
+        } else {
+            $realip = $unknown;
+        }
+    } else {
+        if (getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), $unknown)) {
+            $realip = getenv("HTTP_X_FORWARDED_FOR");
+        } else if (getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), $unknown)) {
+            $realip = getenv("HTTP_CLIENT_IP");
+        } else if (getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), $unknown)) {
+            $realip = getenv("REMOTE_ADDR");
+        } else {
+            $realip = $unknown;
+        }
+    }
+    $realip = preg_match("/[\d\.]{7,15}/", $realip, $matches) ? $matches[0] : $unknown;
+    return $realip;
+}
+
+function GetIpLookup($ip=''){
+    if(empty($ip)){
+        $ip=GetIp();
+    }
+    if($ip=="127.0.0.1"){
+        return "本地localhost";
+    }else{
+        $url='http://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query='.$ip.'&co=&resource_id=6006&oe=utf8';
+        $opts = array('http' =>
+        array(
+        'method'  => 'GET',
+        'timeout' => 3
+        )
+		);
+		$context  = stream_context_create($opts);
+        $reg = @file_get_contents($url,false,$context);
+        if($reg!=null || $reg!==false){
+            $city=$reg['data'][0];
+            return $city['location'];
+        }else{
+            return "未知";
+        }
+    }
+}
+
+//清理缓存函数
+
+/**
+ * 循环删除目录和文件
+ * @param string $dir_name
+ * @return bool
+ */
+function delete_dir_file($dir_name) {
+    $result = false;
+    if(is_dir($dir_name)){
+        if ($handle = opendir($dir_name)) {
+            while (false !== ($item = readdir($handle))) {
+                if ($item != '.' && $item != '..') {
+                    if (is_dir($dir_name . DIRECTORY_SEPARATOR . $item)) {
+                        delete_dir_file($dir_name . DIRECTORY_SEPARATOR . $item);
+                    } else {
+                        unlink($dir_name . DIRECTORY_SEPARATOR . $item);
+                    }
+                }
+            }
+            closedir($handle);
+            if (rmdir($dir_name)) {
+                $result = true;
+            }
+        }
+    }
+    return $result;
+}
+
